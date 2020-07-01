21e3cf1e8a591f2cc53902799c8be2b85ceb3c8a
viper12590 <viper1259@gmail.com>
2020-07-01 15:08:56 +0100

    add fake DB

--- src/test/java/CommunityWebDemo/CommunityWebDemoApplicationTests.java
+++ src/test/java/CommunityWebDemo/CommunityWebDemoApplicationTests.java
@@ -1,10 +1,18 @@
 package CommunityWebDemo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CommunityWebDemoApplicationTests {
+
+	FakeDB fakeDB = new FakeDB();
+
+	@Test
+	void fakeDBInit() {
+		assert (fakeDB.postTable.isEmpty());
+	}
 
 	@Test
 	void contextLoads() {
