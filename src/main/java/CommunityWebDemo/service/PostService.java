26e975e77a87cb436d2762c61513cb1759dc9152
viper12590 <viper1259@gmail.com>
2020-07-01 15:22:34 +0100

    add addPost and getAllPosts

--- src/main/java/CommunityWebDemo/service/PostService.java
+++ src/main/java/CommunityWebDemo/service/PostService.java
@@ -1,5 +1,6 @@
 package CommunityWebDemo.service;
 
+import CommunityWebDemo.FakeDB;
 import CommunityWebDemo.entity.Post;
 import org.springframework.stereotype.Service;
 
@@ -9,11 +10,11 @@ import java.util.List;
 @Service
 public class PostService {
 
-    public List<Post> getAllPosts() {
-        return new ArrayList<>();
+    public List<Post> getAllPosts(FakeDB db) {
+        return db.postTable;
     }
 
-    public void addPost(Post post) {
-
+    public void addPost(FakeDB db, Post post) {
+        db.postTable.add(post);
     }
 }
--- src/test/java/CommunityWebDemo/CommunityWebDemoApplicationTests.java
+++ src/test/java/CommunityWebDemo/CommunityWebDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package CommunityWebDemo;
 
+import CommunityWebDemo.entity.Post;
+import CommunityWebDemo.service.PostService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,9 +11,31 @@ class CommunityWebDemoApplicationTests {
 
 	FakeDB fakeDB = new FakeDB();
 
+	@Autowired
+	PostService postService;
+
 	@Test
 	void fakeDBInit() {
 		assert (fakeDB.postTable.isEmpty());
+	}
+
+	@Test
+	void addPostTest() {
+		Post post0 = new Post((long) 0,"test title","test body", (long) 0);
+		postService.addPost(fakeDB, post0);
+		assert (!fakeDB.postTable.isEmpty());
+		fakeDB.postTable.clear();
+	}
+
+	@Test
+	void getAllPostTest() {
+		Post post0 = new Post((long) 0,"test title","test body", (long) 0);
+		Post post1 = new Post((long) 1,"test title","test body", (long) 1);
+		postService.addPost(fakeDB, post0);
+		postService.addPost(fakeDB, post1);
+		assert (!postService.getAllPosts(fakeDB).isEmpty());
+		assert (postService.getAllPosts(fakeDB).size() == 2);
+		fakeDB.postTable.clear();
 	}
 
 	@Test
