fc26c9793b2347e513d59d641f1b2bfcf7aac8e6
viper12590 <viper1259@gmail.com>
2020-07-02 08:25:52 +0100

    delete FakeDB.java

--- src/main/java/CommunityWebDemo/service/PostService.java
+++ src/main/java/CommunityWebDemo/service/PostService.java
@@ -1,13 +1,8 @@
 package CommunityWebDemo.service;
 
-import CommunityWebDemo.FakeDB;
-import CommunityWebDemo.entity.Post;
 import CommunityWebDemo.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class PostService {
@@ -15,11 +10,4 @@ public class PostService {
     @Autowired
     PostRepository postRepository;
 
-    public List<Post> getAllPosts(FakeDB db) {
-        return db.postTable;
-    }
-
-    public void addPost(FakeDB db, Post post) {
-        db.postTable.add(post);
-    }
 }
--- src/test/java/CommunityWebDemo/CommunityWebDemoApplicationTests.java
+++ src/test/java/CommunityWebDemo/CommunityWebDemoApplicationTests.java
@@ -9,34 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class CommunityWebDemoApplicationTests {
 
-	FakeDB fakeDB = new FakeDB();
-
 	@Autowired
 	PostService postService;
 
-	@Test
-	void fakeDBInit() {
-		assert (fakeDB.postTable.isEmpty());
-	}
-
-	@Test
-	void addPostTest() {
-		Post post0 = new Post((long) 0,"test title","test body", (long) 0);
-		postService.addPost(fakeDB, post0);
-		assert (!fakeDB.postTable.isEmpty());
-		fakeDB.postTable.clear();
-	}
-
-	@Test
-	void getAllPostTest() {
-		Post post0 = new Post((long) 0,"test title","test body", (long) 0);
-		Post post1 = new Post((long) 1,"test title","test body", (long) 1);
-		postService.addPost(fakeDB, post0);
-		postService.addPost(fakeDB, post1);
-		assert (!postService.getAllPosts(fakeDB).isEmpty());
-		assert (postService.getAllPosts(fakeDB).size() == 2);
-		fakeDB.postTable.clear();
-	}
 
 	@Test
 	void contextLoads() {
