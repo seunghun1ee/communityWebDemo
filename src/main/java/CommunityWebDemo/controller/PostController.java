592578f0b102b6d840a1f4ab7fce1225288da07e
viper12590 <viper1259@gmail.com>
2020-07-03 08:49:46 +0100

    /post/{id} works

--- src/main/java/CommunityWebDemo/controller/PostController.java
+++ src/main/java/CommunityWebDemo/controller/PostController.java
@@ -3,12 +3,16 @@ package CommunityWebDemo.controller;
 import CommunityWebDemo.entity.Post;
 import CommunityWebDemo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class PostController {
@@ -22,5 +26,17 @@ public class PostController {
         postService.addPost(new Post("second post", "nice to meet you",1L));
 
         return postService.getAllPosts();
+    }
+
+    @GetMapping("/posts/{id}")
+    public @ResponseBody Post showPostById(@PathVariable Long id) throws Exception {
+        Optional<Post> optionalPost = postService.getPostById(id);
+        Post post;
+        if(optionalPost.isPresent()) {
+            post = optionalPost.get();
+        }
+        else throw new Exception();
+
+        return post;
     }
 }
