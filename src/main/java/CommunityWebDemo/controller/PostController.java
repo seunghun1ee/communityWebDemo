d69c370043b2420fc80b7a6ae49095b6670c208d
viper12590 <viper1259@gmail.com>
2020-07-04 05:53:01 +0100

    deleting post on post request

--- src/main/java/CommunityWebDemo/controller/PostController.java
+++ src/main/java/CommunityWebDemo/controller/PostController.java
@@ -5,10 +5,7 @@ import CommunityWebDemo.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
 import java.util.List;
@@ -51,11 +48,11 @@ public class PostController {
         return new RedirectView("/posts");
     }
 
-    @GetMapping("/posts/{id}/delete")
-    public RedirectView deletePost(@PathVariable Long id) throws Exception{
+    @PostMapping("/posts/{id}/delete")
+    public RedirectView delete(@PathVariable Long id) {
         if(postService.deletePostById(id)) {
             return new RedirectView("/posts");
         }
-        else throw new Exception();
+        else return new RedirectView("/error");
     }
 }
