0ecce081ff1e9406c3ded9c9839fc23d6d97c5d9
viper12590 <viper1259@gmail.com>
2020-07-04 15:33:14 +0100

    Add user entity and repo

---
+++ src/main/java/CommunityWebDemo/entity/User.java
@@ -0,0 +1,13 @@
+package CommunityWebDemo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+    private String name;
+}
