3f489503303466aaca433c84578fc0e22ad0e82e
viper12590 <viper1259@gmail.com>
2020-07-05 09:07:52 +0100

    add test user data

--- src/main/java/CommunityWebDemo/controller/HomeController.java
+++ src/main/java/CommunityWebDemo/controller/HomeController.java
@@ -1,20 +1,24 @@
 package CommunityWebDemo.controller;
 
 import CommunityWebDemo.entity.Post;
+import CommunityWebDemo.entity.User;
 import CommunityWebDemo.repository.PostRepository;
 import CommunityWebDemo.service.PostService;
+import CommunityWebDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
 
     @Autowired
-    PostRepository postRepository;
-    @Autowired
     PostService postService;
+    @Autowired
+    UserService userService;
 
     @GetMapping("/")
     public String helloWorld() {
@@ -23,9 +27,24 @@ public class HomeController {
 
     @GetMapping("/load")
     public @ResponseBody String loadTestData() {
-        postRepository.deleteAll();
-        postService.add(new Post("first post","hello"));
-        postService.add(new Post("second post", "nice to meet you"));
+        postService.deleteAll();
+        userService.deleteAll();
+        User adam = new User("Adam");
+        User eve = new User("Eve");
+        userService.add(adam);
+        userService.add(eve);
+        List<User> users = userService.getAll();
+        for(User user : users) {
+            switch (user.getName()) {
+                case "Adam":
+                    adam = user;
+                case "Eve":
+                    eve = user;
+                default:
+            }
+        }
+        postService.add(new Post("first post","hello", adam));
+        postService.add(new Post("second post", "nice to meet you", eve));
 
         return "Test data is loaded";
     }
