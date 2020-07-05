3bd13b5c7966af621b90c25c0c6333a6766d2e98
viper12590 <viper1259@gmail.com>
2020-07-05 09:38:48 +0100

    read user

--- src/main/java/CommunityWebDemo/controller/UserController.java
+++ src/main/java/CommunityWebDemo/controller/UserController.java
@@ -5,6 +5,7 @@ import CommunityWebDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
@@ -18,5 +19,13 @@ public class UserController {
     @GetMapping("/users")
     public @ResponseBody List<User> showUserList() {
         return userService.getAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public @ResponseBody User showUser(@PathVariable Long id) throws Exception{
+        if(userService.getById(id).isPresent()) {
+            return userService.getById(id).get();
+        }
+        else throw new Exception();
     }
 }
