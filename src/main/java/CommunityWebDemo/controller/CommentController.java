222998711d0d96a235a6739bbad68a5910083abf
viper12590 <viper1259@gmail.com>
2020-07-06 16:00:33 +0100

    be able to add new comment

--- src/main/java/CommunityWebDemo/controller/CommentController.java
+++ src/main/java/CommunityWebDemo/controller/CommentController.java
@@ -1,17 +1,46 @@
 package CommunityWebDemo.controller;
 
+import CommunityWebDemo.entity.Comment;
+import CommunityWebDemo.entity.Post;
 import CommunityWebDemo.entity.User;
 import CommunityWebDemo.repository.CommentRepository;
+import CommunityWebDemo.service.PostService;
+import CommunityWebDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.Optional;
 
 @Controller
 public class CommentController {
 
     @Autowired
+    PostService postService;
+    @Autowired
+    UserService userService;
+    @Autowired
     CommentRepository commentRepository;
 
     User testUser = new User("tester");
+
+    @PostMapping("/posts/{postId}/new_comment")
+    public RedirectView addComment(@PathVariable Long postId, Comment comment) {
+        Optional<Post> optionalPost = postService.getById(postId);
+        if(optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            comment.setPost(post);
+            //temp
+            userService.add(testUser);
+            comment.setUser(testUser);
+            commentRepository.save(comment);
+            return new RedirectView("/posts/{postId}");
+        }
+        else return new RedirectView("/error");
+    }
+
 
 
 }
