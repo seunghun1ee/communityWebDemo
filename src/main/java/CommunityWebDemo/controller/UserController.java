8da53da7658a0c1eba80181e31e00f1fdf35c6ce
viper12590 <viper1259@gmail.com>
2020-07-06 07:12:49 +0100

    add user info page

--- src/main/java/CommunityWebDemo/controller/UserController.java
+++ src/main/java/CommunityWebDemo/controller/UserController.java
@@ -4,6 +4,7 @@ import CommunityWebDemo.entity.User;
 import CommunityWebDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,10 +26,11 @@ public class UserController {
     }
 
     @GetMapping("/users/{id}")
-    public @ResponseBody User showUser(@PathVariable Long id) throws Exception{
+    public String showUser(@PathVariable Long id, Model model) throws Exception{
         Optional<User> optionalUser = userService.getById(id);
         if(optionalUser.isPresent()) {
-            return optionalUser.get();
+            model.addAttribute("user",optionalUser.get());
+            return "user";
         }
         else throw new Exception();
     }
