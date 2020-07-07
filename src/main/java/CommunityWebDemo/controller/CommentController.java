93e4a3ca4a6f1a2ebb5d4ecc19fef240c7530c01
viper12590 <viper1259@gmail.com>
2020-07-07 09:43:47 +0100

    comment editing methods

--- src/main/java/CommunityWebDemo/controller/CommentController.java
+++ src/main/java/CommunityWebDemo/controller/CommentController.java
@@ -8,6 +8,8 @@ import CommunityWebDemo.service.PostService;
 import CommunityWebDemo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
@@ -23,7 +25,7 @@ public class CommentController {
     UserService userService;
     @Autowired
     CommentRepository commentRepository;
-
+    //temp
     User testUser = new User("tester");
 
     @PostMapping("/posts/{postId}/new_comment")
@@ -41,6 +43,27 @@ public class CommentController {
         else return new RedirectView("/error");
     }
 
+    @GetMapping("/posts/{postId}/comments/{commentId}/edit")
+    public String editComment(@PathVariable Long postId, @PathVariable Long commentId, Model model) {
+        Optional<Comment> optionalComment = commentRepository.findById(commentId);
+        if(postService.getById(postId).isPresent() && optionalComment.isPresent()) {
+            model.addAttribute("comment",optionalComment.get());
+            return "updateComment";
+        }
+        return "error";
+    }
 
-
+    @PostMapping("/posts/{postId}/comments/{commentId}/edit")
+    public RedirectView saveEditedComment(@PathVariable Long postId, @PathVariable Long commentId, Comment comment) {
+        Optional<Comment> optionalComment = commentRepository.findById(commentId);
+        if(postService.getById(postId).isPresent() && optionalComment.isPresent()) {
+            Comment oldComment = optionalComment.get();
+            comment.setId(oldComment.getId());
+            comment.setUser(oldComment.getUser());
+            comment.setPost(oldComment.getPost());
+            commentRepository.save(comment);
+            return new RedirectView("/posts/{postId}");
+        }
+        else return new RedirectView("/error");
+    }
 }
