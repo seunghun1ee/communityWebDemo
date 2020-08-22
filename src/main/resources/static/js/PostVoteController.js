b842b023c46b06189f26c1861775d233156f6848
viper12590 <viper1259@gmail.com>
2020-08-22 13:27:47 +0100

    limited vote but unsafe

--- src/main/resources/static/js/PostVoteController.js
+++ src/main/resources/static/js/PostVoteController.js
@@ -1,11 +1,20 @@
+var initialVote = document.getElementById("voteCount");
+var initialVoteNum = parseInt(initialVote.textContent);
+
 function upVote() {
     var voteCount = document.getElementById("voteCount");
     let voteNum = parseInt(voteCount.textContent);
-    voteCount.textContent = (voteNum + 1).toString();
+    if(voteNum < initialVoteNum + 1) {
+        voteCount.textContent = (voteNum + 1).toString();
+    }
+
 }
 
 function downVote() {
     var voteCount = document.getElementById("voteCount");
     let voteNum = parseInt(voteCount.textContent);
-    voteCount.textContent = (voteNum - 1).toString();
+    if(voteNum > initialVoteNum - 1) {
+        voteCount.textContent = (voteNum - 1).toString();
+    }
+
 }
