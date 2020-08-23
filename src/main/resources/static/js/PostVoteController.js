28666e3df775beb2f0ddb13f8439df07dc35a661
viper12590 <viper1259@gmail.com>
2020-08-23 06:01:57 +0100

    new post voting system using 'aria-pressed'

--- src/main/resources/static/js/PostVoteController.js
+++ src/main/resources/static/js/PostVoteController.js
@@ -1,20 +1,36 @@
-var initialVote = document.getElementById("voteCount");
-var initialVoteNum = parseInt(initialVote.textContent);
+const toggles = document.querySelectorAll('[aria-pressed]');
+var voteCount = document.getElementById("voteCount");
+var voteNum = parseInt(voteCount.textContent);
+var upVoteButton = document.getElementById("upVoteButton");
+var downVoteButton = document.getElementById("downVoteButton");
+
+toggles.forEach(toggle => {
+    toggle.addEventListener("click", (e) => {
+        let pressed = e.target.getAttribute("aria-pressed") === "true";
+        e.target.setAttribute("aria-pressed", String(!pressed));
+    })
+})
 
 function upVote() {
-    var voteCount = document.getElementById("voteCount");
-    let voteNum = parseInt(voteCount.textContent);
-    if(voteNum < initialVoteNum + 1) {
+    if(upVoteButton.getAttribute("aria-pressed") !== "true") {
+        //turning toggle on
         voteCount.textContent = (voteNum + 1).toString();
+        downVoteButton.setAttribute("aria-pressed", String(false));
     }
-
+    else {
+        //turning toggle off
+        voteCount.textContent = voteNum.toString();
+    }
 }
 
 function downVote() {
-    var voteCount = document.getElementById("voteCount");
-    let voteNum = parseInt(voteCount.textContent);
-    if(voteNum > initialVoteNum - 1) {
+    if(downVoteButton.getAttribute("aria-pressed") !== "true") {
+        //turning toggle on
         voteCount.textContent = (voteNum - 1).toString();
+        upVoteButton.setAttribute("aria-pressed", String(false));
     }
-
+    else {
+        //turning toggle off
+        voteCount.textContent = voteNum.toString();
+    }
 }
