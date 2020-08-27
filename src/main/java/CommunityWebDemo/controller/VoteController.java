a5b29c2118dd065e62b062e3248978f7850ce95b
viper12590 <viper1259@gmail.com>
2020-08-27 10:02:30 +0100

    remove unused import

--- src/main/java/CommunityWebDemo/controller/VoteController.java
+++ src/main/java/CommunityWebDemo/controller/VoteController.java
@@ -11,7 +11,6 @@ import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
