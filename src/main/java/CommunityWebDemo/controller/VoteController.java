16324bbea341f98c5477cd0c6e731312bcd8b7b8
viper12590 <viper1259@gmail.com>
2020-08-27 09:41:06 +0100

    separate anonymous voting and registered user voting

--- src/main/java/CommunityWebDemo/controller/VoteController.java
+++ src/main/java/CommunityWebDemo/controller/VoteController.java
@@ -2,12 +2,17 @@ package CommunityWebDemo.controller;
 
 import CommunityWebDemo.entity.Post;
 import CommunityWebDemo.entity.Thread;
+import CommunityWebDemo.entity.User;
 import CommunityWebDemo.service.PostService;
 import CommunityWebDemo.service.ThreadService;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,13 +36,30 @@ public class VoteController {
             Post post = optionalPost.get();
             Integer vote = post.getVote();
             JSONObject voterObject = new JSONObject(post.getVoterList());
-            JSONArray votingList = voterObject.getJSONArray("guests");
-            for(int i = 0; i < votingList.length(); i++) {
-                if(votingList.getString(i).equals(request.getRemoteAddr())) {
-                    return "already voted";
+            JSONArray guestVoterList = voterObject.getJSONArray("guests");
+            JSONArray userVoterList = voterObject.getJSONArray("users");
+            //Current user anonymous or registered?
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            //Anonymous
+            if(auth.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ANONYMOUS"))) {
+                for(int i = 0; i < guestVoterList.length(); i++) {
+                    if(guestVoterList.getString(i).equals(request.getRemoteAddr())) {
+                        return "already voted";
+                    }
                 }
+                guestVoterList.put(request.getRemoteAddr());
             }
-
+            //registered
+            else {
+                User currentUser = (User) auth.getPrincipal();
+                for(int i = 0; i < userVoterList.length(); i++) {
+                    if(userVoterList.getLong(i) == currentUser.getId()) {
+                        return "already voted";
+                    }
+                }
+                userVoterList.put(currentUser.getId());
+            }
+            //not voted before
             switch (type) {
                 case "upvote":
                     post.setVote(vote + 1);
@@ -49,7 +71,6 @@ public class VoteController {
                     return "failed";
             }
 
-            votingList.put(request.getRemoteAddr());
             String stringVoteList = voterObject.toString();
             post.setVoterList(stringVoteList);
             postService.add(post);
