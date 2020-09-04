21a5c3126e5206e294096af470266ffe2b552e4d
viper12590 <viper1259@gmail.com>
2020-09-04 10:32:41 +0100

    be able to display posts from subscribed threads after login

--- src/main/java/CommunityWebDemo/controller/HomeController.java
+++ src/main/java/CommunityWebDemo/controller/HomeController.java
@@ -8,7 +8,10 @@ import CommunityWebDemo.entity.User;
 import CommunityWebDemo.repository.ThreadRepository;
 import CommunityWebDemo.service.CommentService;
 import CommunityWebDemo.service.PostService;
+import CommunityWebDemo.service.ThreadService;
 import CommunityWebDemo.service.UserService;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,6 +23,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,15 +40,40 @@ public class HomeController {
     @Autowired
     ThreadRepository threadRepository;
     @Autowired
+    ThreadService threadService;
+    @Autowired
     PasswordEncoder passwordEncoder;
 
     @GetMapping("/")
-    public String helloWorld(Model model) {
+    public String helloWorld(Model model) throws JSONException {
         List<Thread> threads = (List<Thread>) threadRepository.findAll();
         List<Post> posts = postService.getAll();
         posts.sort(new SortByPostVote());
         model.addAttribute("threads",threads);
         model.addAttribute("posts",posts);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(!auth.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ANONYMOUS"))) {
+            User authUser = (User) auth.getPrincipal();
+            JSONObject subscribedThreadsJSON = new JSONObject(authUser.getSubscribedThreads());
+            List<Thread> subscribedThreads = new ArrayList<>();
+            Iterator<String> threadUrls = subscribedThreadsJSON.keys();
+            while (threadUrls.hasNext()) {
+                String threadUrl = threadUrls.next();
+                Optional<Thread> optionalThread = threadService.getByUrl(threadUrl);
+                optionalThread.ifPresent(subscribedThreads::add);
+            }
+            List<Post> subscribedPosts = new ArrayList<>();
+            for(Post post : posts) {
+                for(Thread thread : subscribedThreads) {
+                    if(post.getThread().equals(thread)) {
+                        subscribedPosts.add(post);
+                        break;
+                    }
+                }
+            }
+            subscribedPosts.sort(new SortByPostVote());
+            model.addAttribute("subscribedPosts",subscribedPosts);
+        }
         return "home";
     }
 
