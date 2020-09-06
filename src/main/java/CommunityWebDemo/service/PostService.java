d088b79450c52e304e4ac4a4f5895c5fe0cef13d
viper12590 <viper1259@gmail.com>
2020-09-06 03:35:28 +0100

    new method PostService.incrementViewOfPostById()

--- src/main/java/CommunityWebDemo/service/PostService.java
+++ src/main/java/CommunityWebDemo/service/PostService.java
@@ -78,4 +78,15 @@ public class PostService extends MyService<Post>{
         return posts;
     }
 
+    public boolean incrementViewOfPostById(Long id) {
+        Optional<Post> optionalPost = getById(id);
+        if(optionalPost.isPresent()) {
+            Post post = optionalPost.get();
+            post.setView(post.getView() + 1);
+            postRepository.save(post);
+            return true;
+        }
+        else return false;
+    }
+
 }
