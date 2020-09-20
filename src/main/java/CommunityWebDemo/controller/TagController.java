2023477c939e38d7a8c7130aae073075be56cd51
viper12590 <viper1259@gmail.com>
2020-09-20 08:50:42 +0100

    tag link from thread

--- src/main/java/CommunityWebDemo/controller/TagController.java
+++ src/main/java/CommunityWebDemo/controller/TagController.java
@@ -1,6 +1,5 @@
 package CommunityWebDemo.controller;
 
-import CommunityWebDemo.entity.Comment;
 import CommunityWebDemo.entity.Post;
 import CommunityWebDemo.entity.Tag;
 import CommunityWebDemo.entity.Thread;
@@ -22,7 +21,6 @@ import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.springframework.web.servlet.view.RedirectView;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Controller
